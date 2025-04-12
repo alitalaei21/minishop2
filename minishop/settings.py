f45794a6e8ac313fd9cb3d9a1b866b76d7da0e03
ameer <am.esmaeeili@gmail.com>
2025-04-12 21:57:09 +0330

    Add media configuration

--- minishop/settings.py
+++ minishop/settings.py
@@ -15,8 +15,14 @@ from datetime import timedelta
 import psycopg2
 from pathlib import Path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
