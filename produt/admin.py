f4e8f2538830c2cc034cdb67a6b756f07011149c
ameer <am.esmaeeili@gmail.com>
2025-04-12 22:18:23 +0330

    add admin baner

--- produt/admin.py
+++ produt/admin.py
@@ -2,10 +2,11 @@ from itertools import product
 
 from django.contrib import admin
 
-from produt.models import Product, Category, Order, OrderItem
+from produt.models import Product, Category, Order, OrderItem, Baner
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Baner)
